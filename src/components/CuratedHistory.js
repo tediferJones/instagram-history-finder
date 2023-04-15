24151ee2960217562fe4fcc9a017cc12036ea5e1
ted <theo_drz@live.com>
2023-04-15 19:45:16 -0400

    styled CuratedHistory component

--- src/components/CuratedHistory.js
+++ src/components/CuratedHistory.js
@@ -23,7 +23,9 @@ function CuratedHistory(props) {
   }
 
   const curatedHistory = props.history.filter(filterHistoryByTime).filter(filterHistoryByAuthor);
-
+  {/*
+    flex flex-col items-center
+  */}
   return (
     <div>
       <h1 className='text-3xl p-8'
@@ -31,18 +33,16 @@ function CuratedHistory(props) {
       <div className='flex flex-col items-center'>
         {curatedHistory.map(item => {
           return (
-            <div className='p-4'
+            <div className='p-4 m-8 w-full lg:w-3/5 flex duration-700 border-black hover:bg-gray-600 hover:text-white hover:border-white'
               key={uuidv4()}
             >
-              {item.author === 'DELETED' ? <h3>DELETED</h3> : 
-              <a href={`https://www.instagram.com/${item.author}`}>
+              {item.author === 'DELETED' ? <h3 className='flex-1 text-center'>DELETED</h3> : 
+              <a href={`https://www.instagram.com/${item.author}`} className='flex-1 text-center'>
                 <h3>@{item.author}</h3>
               </a>
               }
-              <div className='flex justify-between gap-4'>
-                <h3>{item.time}</h3>
-                <h3>{item.postType}</h3>
-              </div>
+              <h3 className='flex-1 text-center border-current border-l-2'>{item.time}</h3>
+              <h3 className='flex-1 text-center border-current border-l-2'>{item.postType}</h3>
             </div>
           )
         })}
